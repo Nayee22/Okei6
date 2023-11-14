50b8bd52a35165ee07660d4b68d0a54924997b6a
Nayee22 <140297710+Nayee22@users.noreply.github.com>
2023-11-14 01:51:00 -0500

    Update fun-ship.js

--- plugins/fun-ship.js
+++ plugins/fun-ship.js
@@ -5,7 +5,7 @@ function handler(m, { groupMetadata }) {
     let b
     do b = ps.getRandom()
     while (b === a)
-    m.reply(`${toM(a)} ❤️ ${toM(b)}\nCongratulations 💖🍻`, null, {
+    m.reply(`${toM(a)} ❤️ ${toM(b)}\nfelicidades ahora son la nueva pareja amorosa del grupo 💖🍻`, null, {
         mentions: [a, b]
     })
 }
